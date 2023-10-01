88c0e2d0bacc50ff3d8ac7bbeab35568bc7ff63e
RichardPajzinka <rpajzinka@gmail.com>
2023-10-01 14:18:55 +0000

    routing_flask

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ flask_app = Flask(__name__)
 @flask_app.route("/")
 def index():
 	return "hello world"
+
+@flask_app.route("/admin/")
+def view_admin():
+	return"hello z inej url:"
+
+@flask_app.route("/admin/<string:name>/")
+def view_admin_name(name):
+	return"hello z URL: {}".format(name)
