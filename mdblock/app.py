9f78aa0b0f76fbbb70c2206bd9b76f13806d0fd8
RichardPajzinka <rpajzinka@gmail.com>
2023-10-08 12:32:12 +0000

    pridanie article a articles

--- mdblock/app.py
+++ mdblock/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 
+from .database import articles
+
 flask_app = Flask(__name__)
 
 @flask_app.route("/")
@@ -15,9 +17,16 @@ def about():
 def admin_page():
 	return render_template("admin.jinja")
 
-@flask_app.route("/article/")
-def article():
-	return render_template("article.jinja")
+@flask_app.route("/articles/")
+def view_articles():
+	return render_template("articles.jinja", articles=articles.items())
+
+@flask_app.route("/articles/<int:art_id>")
+def view_article(art_id):
+	article = articles.get(art_id)
+	if article:
+		return render_template("article.jinja", article=article)
+	return render_template("article_nenajdeny.jinja", art_id=art_id)
 
 #@flask_app.route("/admin/")
 #def view_admin():
