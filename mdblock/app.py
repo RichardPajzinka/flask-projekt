0f1b37c7d5ef048f0a344b25b7b904b8f4586dc2
RichardPajzinka <rpajzinka@gmail.com>
2023-10-29 11:53:17 +0000

    pridanie_uprav

--- mdblock/app.py
+++ mdblock/app.py
@@ -6,7 +6,12 @@ from flask import url_for
 from flask import session
 from flask import g
 from flask import flash
-
+#zakladna trieda na vytvaranie formularov z tejto triedy budeme dedit nove formulare
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms import PasswordField
+from wtforms import TextAreaField
+from wtforms.validators import InputRequired
 #from .database import articles
 
 import sqlite3
@@ -22,6 +27,17 @@ flask_app.config.from_pyfile("/vagrant/configs/default.py")
 if "MDBLOCK_CONFIG" in os.environ:
 	flask_app.config.from_envvar("MDBLOCK_CONFIG")
 
+
+#Forms
+class LoginForm(FlaskForm):
+	username = StringField("Username", validators=[InputRequired()])
+	password = PasswordField("Password", validators=[InputRequired()])
+
+class ArticleForm(FlaskForm):
+	title = StringField("Title", validators=[InputRequired()])
+	content = TextAreaField("Content")
+
+#
 @flask_app.route("/")
 def welcome_page():
 	return render_template("welcome_page.html")
@@ -44,8 +60,20 @@ def view_articles():
 	articles = cur.fetchall()
 	return render_template("articles.jinja", articles=articles)
 
+@flask_app.route("/articles/new", methods=["GET"])
+def view_add_article():
+	#ak nie osm prihlaseni
+	if "logged" not in session:
+		return redirect(url_for("view_login"))
+	
+	form = ArticleForm()
+	return render_template("article_editor.jinja", form=form)
+
 @flask_app.route("/articles/", methods=["POST"])
 def add_articles():
+	if "logged" not in session:
+		return redirect(url_for("view_login"))
+
 	db = get_db()
 	db.execute("insert into articles (title,content) values (?,?)",
 		[request.form.get("title"), request.form.get("content")])
@@ -55,22 +83,61 @@ def add_articles():
 @flask_app.route("/articles/<int:art_id>")
 def view_article(art_id):
 	db = get_db()
-	cur = db.execute("select * from where if=(?)",[art_id])
+	cur = db.execute("select * from articles where if=(?)",[art_id])
 	article = cur.fetchone()
 	if article:
 		return render_template("article.jinja", article=article)
 	return render_template("article_nenajdeny.jinja", art_id=art_id)
 
+@flask_app.route("/articles/<int:art_id>/edit/", methods=["GET"])
+def view_article_editor(art_id):
+	if "logged" not in session:
+		return redirect(url_for("view_login"))
+	db = get_db()
+	cur = db.execute("select * from articles where if=(?)",[art_id])
+	article = cur.fetchone()
+	if article:
+		form = ArticleForm()
+		form.title.data = article["title"]
+		form.content.data = article["content"]
+		return render_template("article_editor.jinja", form=form, article=article)
+	return render_template("article_nenajdeny.jinja", art_id=art_id)
+
+@flask_app.route("/articles/<int:art_id>/", methods=["POST"])
+def edit_article(art_id):
+	#riesim ci som prihlaseny
+	if "logged" not in session:
+		return redirect(url_for("view_login"))
+	#skontorlujem ci sa clanok nachadza v DB
+	db = get_db()
+	cur = db.execute("select * from articles where if=(?)",[art_id])
+	article = cur.fetchone()
+	if article:
+		edit_form = ArticleForm(request.form)
+		if edit_form.validate():
+			#pass
+			db.execute("update articles set title=?, content=? where if=?",
+				[edit_form.title.data, edit_form.content.data, art_id])
+			db.commit()
+			flash("Editovanie ulozene","alert-success")
+			return redirect(url_for("view_article", art_id=art_id))
+		else:
+			for error in login_form.errors:
+				flask("{} chyba".format(error), "alert-danger")
+			return redirect(url_for("view_login")) 
+
 @flask_app.route("/login/", methods=["GET"])
 def view_login():
-	return render_template("login.jinja")
+	login_form = LoginForm()
+	return render_template("login.jinja", form=login_form)
 
 @flask_app.route("/login/", methods=["POST"])
 def login_user():
 	#if request.method == "POST":
-		username = request.form["username"]
-		password = request.form["password"]
-		if username == flask_app.config["USERNAME"] and password == flask_app.config["PASSWORD"]:
+	login_form = LoginForm(request.form)
+	if login_form.validate():
+		if login_form.username.data == flask_app.config["USERNAME"] and \
+			login_form.password.data == flask_app.config["PASSWORD"]:
 			#return "OK"
 			session["logged"] = True
 			flash("Login successful", "alert-success")
@@ -79,6 +146,11 @@ def login_user():
 			flash("Invalid successful", "alert-danger")
 			#return "NOT OK"
 			return redirect(url_for("view_login"))
+	else:
+		for error in login_form.errors:
+			flask("{} chyba".format(error), "alert-danger")
+		return redirect(url_for("view_login")) 
+
 
 @flask_app.route("/logout/", methods=["POST"])
 def logout_user():
